7559cca67ec4df2449ff8fe25e7d796c0672c6bc
Bob Beilstein <bob.beilstein@jda.com>
2014-12-03 21:07:19 -0500

    add a bit of actual programming, just for fun

--- foo.pl
+++ foo.pl
@@ -3,3 +3,7 @@ print "Howdy, Earth, Mars Calling\n";
 print "Whatever you'd like to do is fine with me!!\n";
 print "wheeree\n";
 print "Wheeeeeeeeeeeeeee\n";
+for $in (1..3) {
+	print "$in\n";
+	}
+
